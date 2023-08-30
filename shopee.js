d3fbef35a72ce47ae0735dd27cf29bb424d42e2a
Wahyu <99200257+wahyu98apr@users.noreply.github.com>
2023-08-31 04:38:04 +0700

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -1,95 +1,86 @@
-var angk = Math.floor(Math.random() * 9) + 1;  
-					if(angk =="1"){
-	imgPrdct="https://down-id.img.susercontent.com/file/id-11134207-7quky-lh6gfhlxthgw28";
-    linkPrdct="https://shope.ee/8KMvOAdw2E";
-    				} else if(angk =="2"){
-	imgPrdct="https://down-id.img.susercontent.com/file/1e1ad9f17df6e6b041189bb2dea7ca47";
-    linkPrdct="https://shope.ee/8UgLaTdIhH";
-					} else if(angk =="3"){
-	imgPrdct="https://down-id.img.susercontent.com/file/6b3e927d5ef8df5a527aea8339368203";
-    linkPrdct="https://shope.ee/8ezlmmcfMK";
-					} else if(angk =="4"){
-	imgPrdct="https://down-id.img.susercontent.com/file/1e8f2a93655d9a1307836b7bdc05a003";
-    linkPrdct="https://shope.ee/8pJBz5c21N";
-					} else if(angk =="5"){
-	imgPrdct="https://down-id.img.susercontent.com/file/sg-11134201-23020-brst5v4ak4mvf6";
-    linkPrdct="https://shope.ee/8zccBObOgQ";
-					} else if(angk =="6"){
-	imgPrdct="https://down-id.img.susercontent.com/file/824b98dbd14e548617ab074dbe610d54";
-    linkPrdct="https://shope.ee/99w2NhalLT";
-					} else if(angk =="7"){
-	imgPrdct="https://down-id.img.susercontent.com/file/622fe46b1f968374198615a2182fd32a";
-    linkPrdct="https://shope.ee/1LDB3J4aEy";
-					} else if(angk =="8"){
-	imgPrdct="https://down-id.img.susercontent.com/file/7ea00d0d0f484a5769e83dada03221ce";
-    linkPrdct="https://shope.ee/1VWbFc3wu1";
-					} else if(angk =="9"){
-	imgPrdct="https://down-id.img.susercontent.com/file/c4fc699fe1e1079c4cedb2d4a9c23bc7";
-    linkPrdct="https://shope.ee/1fq1Rv3JZ4";
-					} else if(angk =="10"){
-	imgPrdct="https://down-id.img.susercontent.com/file/id-11134207-23020-adopy8fo7lnvcd";
-    linkPrdct="https://shope.ee/1q9ReE2gE7";
-					}  
+      // Create interstitial
+        const interstitial = document.createElement('div');
+        interstitial.className = 'interstitial';
+        
+        // Create close button container
+        const closeButtonContainer = document.createElement('div');
+        closeButtonContainer.className = 'close-button-container';
+        
+        // Create promo label
+        const promoLabel = document.createElement('div');
+        promoLabel.className = 'promo-label';
+        promoLabel.textContent = 'Promo Obral Murah';
+        
+        // Create close button
+        const closeButton = document.createElement('button');
+        closeButton.className = 'close-button';
+        closeButton.id = 'closeButton';
+        closeButton.textContent = 'Close';
+        
+        // Append promo label and close button to close button container
+        closeButtonContainer.appendChild(promoLabel);
+        closeButtonContainer.appendChild(closeButton);
+        
+        // Create scrollable container
+        const scrollableContainer = document.createElement('div');
+        scrollableContainer.className = 'scrollable-container';
+        scrollableContainer.id = 'dataContainer';
+        
+        // Append close button container and scrollable container to interstitial
+        interstitial.appendChild(closeButtonContainer);
+        interstitial.appendChild(scrollableContainer);
+        
+        // Append interstitial to body
+        document.body.appendChild(interstitial);  
+    
+    
+    
+    
+        // URL of the JSON data
+        const url = 'https://wahyu98apr.github.io/shopee.json';
 
-    // Create the interstitial container
-    var interstitial = document.createElement('div');
-    interstitial.id = 'interstitial';
-    interstitial.style.display = 'none';
-    interstitial.style.position = 'fixed';
-    interstitial.style.top = '0';
-    interstitial.style.left = '0';
-    interstitial.style.width = '100%';
-    interstitial.style.height = '100%';
-    interstitial.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-    document.body.appendChild(interstitial);
 
-    // Create the interstitial link element
-    var interstitialLink = document.createElement('a');
-    interstitialLink.href = linkPrdct;
-    interstitialLink.target = '_blank'; 
-    interstitial.appendChild(interstitialLink);
 
-    // Create the interstitial image element
-    var interstitialImage = document.createElement('img');
-    interstitialImage.id = 'interstitialImage';
-    interstitialImage.style.display = 'block';
-    interstitialImage.style.margin = 'auto';
-    interstitialImage.style.marginTop = '10%';
-    interstitialImage.style.maxWidth = '80%';
-    interstitialImage.style.maxHeight = '80%';
-    interstitialImage.style.border = '10px solid #ee4d2d';
-    interstitialImage.src = imgPrdct;
-    interstitial.style.display = 'block';
-    interstitialLink.appendChild(interstitialImage);
-	
-    // Create the additional image element at the bottom
-    var additionalImage = document.createElement('img');
-    additionalImage.src = 'https://media.suara.com/pictures/653x366/2020/10/26/25067-shopee.jpg';
-    additionalImage.style.position = 'fixed';
-    additionalImage.style.bottom = '0';
-    additionalImage.style.left = '50%';
-    additionalImage.style.transform = 'translateX(-50%)';
-    additionalImage.style.maxWidth = '480px';
-    additionalImage.style.width = '100%';
-    document.body.appendChild(additionalImage);
+        // Open the interstitial
+        interstitial.style.display = 'flex';
 
-    // Create the close button for the interstitial
-    var closeButton = document.createElement('span');
-    closeButton.id = 'closeButton';
-    closeButton.innerHTML = '&times;';
-    closeButton.style.position = 'absolute';
-    closeButton.style.top = '10px';
-    closeButton.style.right = '10px';
-    closeButton.style.cursor = 'pointer';
-    closeButton.style.color = '#fff';
-    closeButton.style.fontSize = '50px';
-    interstitial.appendChild(closeButton);
+        // Fetch JSON data using the fetch() API
+        fetch(url)
+            .then(response => response.json()) // Parse the response as JSON
+            .then(data => {
+                // Randomly shuffle the data array
+                const shuffledData = data.sort(() => 0.5 - Math.random());
 
-    // Close the interstitial when the close button is clicked
-    closeButton.onclick = function() {
-      interstitial.style.display = 'none';
-      additionalImage.style.display = 'none';
-    };
+                // Select the first 4 items from the shuffled array
+                const selectedItems = shuffledData.slice(0, 20);
 
-    // additionalImage is clicked
-    additionalImage.onclick = function() {window.location=linkPrdct};
+                // Create a card for each selected data item
+selectedItems.forEach(item => {
+    const card = document.createElement('div');
+    card.className = 'product-card';
+    card.innerHTML = `
+		<a class="lProd" href="${item.LinkKomisiEkstra}" target="_blank">
+			<img class="product-image" src="${item.Image}" alt="${item.NamaProduk}" title="${item.NamaProduk}">
+			<div class="product-details">
+				<p> ${item.NamaProduk}</p>
+				<p class="harga"> ${item.Harga}</p>
+				<p> ${item.NamaToko}</p>
+            
+ 
+			</div>
+		</a>
+    `;
+    dataContainer.appendChild(card);
+});
+
+                
+            })
+            .catch(error => {
+                console.error('Error fetching JSON:', error);
+                dataContainer.textContent = 'Error fetching JSON data';
+            });
+
+        // Close the interstitial when the close button is clicked
+        closeButton.addEventListener('click', () => {
+            interstitial.style.display = 'none';
+        });
